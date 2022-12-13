4de19533e915364a184eee3d60459dd967a51e6f
gabi0702 <gabi.nakache2015@gmail.com>
2022-12-13 18:08:11 +0200

    withDisableBtn

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Checker from "./components/Checker";
 
 function App() {
   const [val, setVal] = useState("");
-
+  const [preventSubmit, setPreventSubmit] = useState(true);
   function valueChanged(e) {
     setVal(e.target.value);
     console.log(val);
   }
-
+  function setPrev(prev) {
+    setPreventSubmit(prev);
+  }
   return (
     <div className="App">
       <h1>Password Checker</h1>
@@ -21,10 +23,18 @@ function App() {
           required
           onChange={valueChanged}
         />
-        <Checker val={val} />
+        <Checker val={val} setPrev={setPrev} />
       </div>
 
-      <button className="submit-btn">Submit</button>
+      <button
+        disabled={preventSubmit}
+        className="submit-btn"
+        onClick={() => {
+          console.log("clicked");
+        }}
+      >
+        Submit
+      </button>
       <h3 id={val.length === 0 ? "hide-password" : "show-password"}>
         Your Password is: {val}
       </h3>
--- src/components/Checker.js
+++ src/components/Checker.js
@@ -4,22 +4,28 @@ import "../App.js";
 const Checker = (props) => {
   const value = props.val;
   const [message, setMessage] = useState("");
-
+  const setPrev = props.setPrev;
   useEffect(() => {
     if (value.length <= 2 && value.length > 0) {
       setMessage("Week Password");
+      setPrev(true);
       document.getElementById("strength-checker").style.background = "red";
       document.getElementById("strength-checker").style.width = "30px";
     } else if (value.length <= 5 && value.length > 2) {
       setMessage("Medium Password");
+      setPrev(false);
+
       document.getElementById("strength-checker").style.background = "orange";
       document.getElementById("strength-checker").style.width = "60px";
     } else if (value.length > 5) {
       setMessage("Strong Password");
+      setPrev(false);
+
       document.getElementById("strength-checker").style.background = "green";
       document.getElementById("strength-checker").style.width = "100px";
     } else {
       setMessage("");
+      setPrev(true);
       document.getElementById("strength-checker").style.background =
         "transparent";
     }
